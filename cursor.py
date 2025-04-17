1b164f39620ccad25f31edc1c8cd852952259e68
mahir-crore <manjityadav0189@gmail.com>
2025-04-17 13:44:47 +0530

    cursor.py

--- cursor.py
+++ cursor.py
@@ -15,7 +15,7 @@ while True:
 
 ## Set all curser according to your sccreen 
 
-
+"""
 first_chat_click = (280, 520)
 chat_copy_drag = "(972,202 to 2213, 1278)"
 first_msg_chatgpt= (515, 347)
@@ -24,6 +24,7 @@ drag_chatgpt_reply= "(858, 158 to 828, 681)"
 copy_reply_chatgpt=(426,522),(427,473),#first time msg 
 copy_reply_chatgpt=(427,238),(427,269),(427,297)#second or all  time msg 
 send_reply= (628, 694)
+"""
 
     
 
